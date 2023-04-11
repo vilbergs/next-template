b605cbe6f9023ffdca08bd330b3d4ab8c751352b
Vilberg <vilbergs@gmail.com>
2023-04-11 12:45:52 +0200

    Add forms plugin and configure theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
-
