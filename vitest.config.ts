d37745dcef8a53663c8aeae5a39f8173fc279cc8
Vilberg <vilbergs@gmail.com>
2023-04-12 16:00:04 +0200

    Fix paths for src dir

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   resolve: {
     alias: {
-      '@/': '/',
+      '@/': '/src/',
+      'test/': '/test/',
     },
   },
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    includeSource: ['./**/*.{js,ts,tsx}'],
+    includeSource: ['./src/**/*.{js,ts,tsx}'],
     setupFiles: ['./test/unit/setup.ts'],
   },
 })
